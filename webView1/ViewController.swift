ca4686a21bc30dd16883313e34af396b79c65ad0
Dhana Lakshmi <dhana.dasa12345@achiever-93.local>
2018-07-10 12:03:06 +0530

    sdygfuhjsdrrghjgk

--- webView1/ViewController.swift
+++ webView1/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var webViewObj: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        webViewObj.loadRequest(URLRequest(url: URL(string:"https://www.google.com")!))
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
