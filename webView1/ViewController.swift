17712c146313db8f3d0b2513ea5d6bf311e76fb8
Dhana Lakshmi <dhana.dasa12345@achiever-93.local>
2018-07-10 11:29:50 +0530

    Initial Commit

---
+++ webView1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webView1
+//
+//  Created by Dhana Lakshmi on 7/10/18.
+//  Copyright © 2018 brn.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
